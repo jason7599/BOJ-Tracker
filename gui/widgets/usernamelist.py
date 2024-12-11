8e28b31cda20ba15c83a0eb1c1d30cc4b542fe95
jason7599 <jason7599@hanmail.net>
2024-12-11 11:42:01 +0900

    disable editing userlist

--- gui/widgets/usernamelist.py
+++ gui/widgets/usernamelist.py
@@ -1,12 +1,17 @@
-from PyQt5.QtWidgets import QListView
+from PyQt5.QtWidgets import QListView, QAbstractItemView
 from PyQt5.QtGui import QStandardItem, QStandardItemModel
 
 # TODO: prettier entries
 class UsernameList(QListView):
     def __init__(self, parent=None):
         super().__init__(parent)
+        
+        # disable edit
+        self.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
+        
         self.setModel(QStandardItemModel(self))
 
+    # TODO: think of a more efficient way
     def is_username_listed(self, username: str) -> bool:
         for i in range(self.model().rowCount()):
             if self.model().item(i).text() == username:
